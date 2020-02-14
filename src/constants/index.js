fbd5198255ed10142c204f06c387e0e3b802ab3c
Eduardo Rotundaro <eduardorotundaro@gmail.com>
2020-02-14 00:22:51 -0300

    :sparkles: Arrows added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,59 +1,170 @@
 export const ARROWS = [
-    { name: 'Leftwards Arrow', html: '8592', hex: '2190', entity: 'larr' },
+    { html: '8592', hex: '2190', entity: 'larr' },
+    { html: '8593', hex: '2191', entity: 'uarr' },
+    { html: '8594', hex: '2192', entity: 'rarr' },
+    { html: '8595', hex: '2193', entity: 'darr' },
+    { html: '8596', hex: '2194', entity: 'harr' },
+    { html: '8597', hex: '2195', entity: '' },
+    { html: '8598', hex: '2196', entity: '' },
+    { html: '8599', hex: '2197', entity: '' },
+    { html: '8600', hex: '2198', entity: '' },
+    { html: '8601', hex: '2199', entity: '' },
+    { html: '8602', hex: '219A', entity: '' },
+    { html: '8603', hex: '219B', entity: '' },
+    { html: '8604', hex: '219C', entity: '' },
+    { html: '8605', hex: '219D', entity: '' },
+    { html: '8606', hex: '219E', entity: '' },
+    { html: '8607', hex: '219F', entity: '' },
+    { html: '8608', hex: '21A0', entity: '' },
+    { html: '8609', hex: '21A1', entity: '' },
+    { html: '8610', hex: '21A2', entity: '' },
+    { html: '8611', hex: '21A3', entity: '' },
+    { html: '8612', hex: '21A4', entity: '' },
+    { html: '8613', hex: '21A5', entity: '' },
+    { html: '8614', hex: '21A6', entity: '' },
+    { html: '8615', hex: '21A7', entity: '' },
+    { html: '8616', hex: '21A8', entity: '' },
+    { html: '8617', hex: '21A9', entity: '' },
+    { html: '8618', hex: '21AA', entity: '' },
+    { html: '8619', hex: '21AB', entity: '' },
+    { html: '8620', hex: '21AC', entity: '' },
+    { html: '8621', hex: '21AD', entity: '' },
+    { html: '8622', hex: '21AE', entity: '' },
+    { html: '8623', hex: '21AF', entity: '' },
+    { html: '8624', hex: '21B0', entity: '' },
+    { html: '8625', hex: '21B1', entity: '' },
+    { html: '8626', hex: '21B2', entity: '' },
+    { html: '8627', hex: '21B3', entity: '' },
+    { html: '8628', hex: '21B4', entity: '' },
+    { html: '8629', hex: '21B5', entity: 'crarr' },
+    { html: '8630', hex: '21B6', entity: '' },
+    { html: '8631', hex: '21B7', entity: '' },
+    { html: '8632', hex: '21B8', entity: '' },
+    { html: '8633', hex: '21B9', entity: '' },
+    { html: '8634', hex: '21BA', entity: '' },
+    { html: '8635', hex: '21BB', entity: '' },
+    { html: '8636', hex: '21BC', entity: '' },
+    { html: '8637', hex: '21BD', entity: '' },
+    { html: '8638', hex: '21BE', entity: '' },
+    { html: '8639', hex: '21BF', entity: '' },
+    { html: '8640', hex: '21C0', entity: '' },
+    { html: '8641', hex: '21C1', entity: '' },
+    { html: '8642', hex: '21C2', entity: '' },
+    { html: '8643', hex: '21C3', entity: '' },
+    { html: '8644', hex: '21C4', entity: '' },
+    { html: '8645', hex: '21C5', entity: '' },
+    { html: '8646', hex: '21C6', entity: '' },
+    { html: '8647', hex: '21C7', entity: '' },
+    { html: '8648', hex: '21C8', entity: '' },
+    { html: '8649', hex: '21C9', entity: '' },
+    { html: '8650', hex: '21CA', entity: '' },
+    { html: '8651', hex: '21CB', entity: '' },
+    { html: '8652', hex: '21CC', entity: '' },
+    { html: '8653', hex: '21CD', entity: '' },
+    { html: '8654', hex: '21CE', entity: '' },
+    { html: '8655', hex: '21CF', entity: '' },
+    { html: '8656', hex: '21D0', entity: 'lArr' },
+    { html: '8657', hex: '21D1', entity: 'uArr' },
+    { html: '8658', hex: '21D2', entity: 'rArr' },
+    { html: '8659', hex: '21D3', entity: 'dArr' },
+    { html: '8660', hex: '21D4', entity: 'hArr' },
+    { html: '8661', hex: '21D5', entity: '' },
+    { html: '8662', hex: '21D6', entity: '' },
+    { html: '8663', hex: '21D7', entity: '' },
+    { html: '8664', hex: '21D8', entity: '' },
+    { html: '8665', hex: '21D9', entity: '' },
+    { html: '8666', hex: '21DA', entity: '' },
+    { html: '8667', hex: '21DB', entity: '' },
+    { html: '8668', hex: '21DC', entity: '' },
+    { html: '8669', hex: '21DD', entity: '' },
+    { html: '8670', hex: '21DE', entity: '' },
+    { html: '8671', hex: '21DF', entity: '' },
+    { html: '8672', hex: '21E0', entity: '' },
+    { html: '8673', hex: '21E1', entity: '' },
+    { html: '8674', hex: '21E2', entity: '' },
+    { html: '8675', hex: '21E3', entity: '' },
+    { html: '8676', hex: '21E4', entity: '' },
+    { html: '8677', hex: '21E5', entity: '' },
+    { html: '8678', hex: '21E6', entity: '' },
+    { html: '8679', hex: '21E7', entity: '' },
+    { html: '8680', hex: '21E8', entity: '' },
+    { html: '8681', hex: '21E9', entity: '' },
+    { html: '8682', hex: '21EA', entity: '' },
+    { html: '8683', hex: '21EB', entity: '' },
+    { html: '8684', hex: '21EC', entity: '' },
+    { html: '8685', hex: '21ED', entity: '' },
+    { html: '8686', hex: '21EE', entity: '' },
+    { html: '8687', hex: '21EF', entity: '' },
+    { html: '8688', hex: '21F0', entity: '' },
+    { html: '8689', hex: '21F1', entity: '' },
+    { html: '8690', hex: '21F2', entity: '' },
+    { html: '8691', hex: '21F3', entity: '' },
+    { html: '8692', hex: '21F4', entity: '' },
+    { html: '8693', hex: '21F5', entity: '' },
+    { html: '8694', hex: '21F6', entity: '' },
+    { html: '8695', hex: '21F7', entity: '' },
+    { html: '8696', hex: '21F8', entity: '' },
+    { html: '8697', hex: '21F9', entity: '' },
+    { html: '8698', hex: '21FA', entity: '' },
+    { html: '8699', hex: '21FB', entity: '' },
+    { html: '8700', hex: '21FC', entity: '' },
+    { html: '8701', hex: '21FD', entity: '' },
+    { html: '8702', hex: '21FE', entity: '' },
+    { html: '8703', hex: '21FF', entity: '' }
 ];
 
 export const BLOCK = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const BOX = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const CURRENCY = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const GEOMETRIC = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const MATH = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const MISC = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const SMILEYS_PEOPLE = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const ANIMALS_NATURE = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const FOOD_DRINK = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const ACTIVITY = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const TRAVEL_PLACES = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const OBJECTS = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const SYMBOLS = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
 
 export const FLAGS = [
-    { name: '', html: '', hex: '', entity: '' },
+    { html: '', hex: '', entity: '' },
 ];
