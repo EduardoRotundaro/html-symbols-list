e6c9189fb7d7ea1b6479f54a0eab8727c57f69d8
Eduardo Rotundaro <eduardorotundaro@gmail.com>
2020-02-13 23:26:43 -0300

    :sparkles: 404 page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export default () => (
     <section>
-        <div>
-            404
+        <div className='not-found'>
+            <div className='error-text'>
+                <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404"/>
+                <span>404</span>
+            </div>
         </div>
     </section>
 );
